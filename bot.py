26a0c7c58b47ab2c08afca61a36d94e98fc62743
dipakmori0 <your_email@example.com>
2025-09-02 06:04:39 +0530

    Bot ready for Render deployment

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from telebot import TeleBot
 
 BOT_TOKEN = "8277485140:AAERBu7ErxHReWZxcYklneU1wEXY--I_32c"
-
 bot = TeleBot(BOT_TOKEN)
 bot.remove_webhook()  # Prevents 409 conflict
 
 print("🤖 Bot is starting...")
+bot.infinity_polling()
